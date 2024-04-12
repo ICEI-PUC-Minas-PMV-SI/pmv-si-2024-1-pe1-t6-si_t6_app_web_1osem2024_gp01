03f9f1dea7c5bf8967ccd8699fb259e36b98d768
Ingridbeca <inggmyg@gmail.com>
2024-04-12 10:39:20 -0300

    Tela do Histórico Estático

---
+++ codigo/script/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    // Inicialize sua tabela DataTable
+    $('#table').DataTable({
+        // Desativar paginação
+        paging: false,
+        // Desativar barra de pesquisa
+        searching: false
+    });
+});
