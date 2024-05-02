93c96bfa0599578336f5846c25e9ba5070108fe6
Ingridbeca <inggmyg@gmail.com>
2024-05-02 09:30:11 -0300

    historico sendo atualizado com os dados de Receita ou Despesa cadastrados

--- src/script/index.js
+++ src/script/index.js
@@ -1,9 +1,36 @@
 $(document).ready(function() {
-    // Inicialize sua tabela DataTable
     $('#table').DataTable({
-        // Desativar paginação
-        paging: false,
-        // Desativar barra de pesquisa
-        searching: false
+        "language": {
+            "sEmptyTable":     "Nenhum dado disponível na tabela",
+            "sInfo":           "Mostrando de _START_ até _END_ de _TOTAL_ entradas",
+            "sInfoEmpty":      "Mostrando 0 até 0 de 0 entradas",
+            "sInfoFiltered":   "(filtrado de _MAX_ entradas totais)",
+            "sInfoPostFix":    "",
+            "sInfoThousands":  ".",
+            "sLengthMenu":     "_MENU_ resultados por página",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing":     "Processando...",
+            "sSearch":         "Buscar:",
+            "sZeroRecords":    "Nenhum registro encontrado",
+            "oPaginate": {
+                "sFirst":    "Primeiro",
+                "sLast":     "Último",
+                "sNext":     "Próximo",
+                "sPrevious": "Anterior"
+            },
+            "oAria": {
+                "sSortAscending":  ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            },
+            "select": {
+                "rows": {
+                    "_": "%d linhas selecionadas",
+                    "0": "Nenhuma linha selecionada",
+                    "1": "1 linha selecionada"
+                }
+            }
+        }
     });
 });
+
+
