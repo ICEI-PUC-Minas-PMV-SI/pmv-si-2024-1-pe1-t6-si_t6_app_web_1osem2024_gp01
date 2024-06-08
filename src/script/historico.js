1918c9177b08cf9026a05a89efd842f0f4c90898
inggmyg@gmail.com <inggmyg@gmail.com>
2024-06-07 21:15:27 -0300

    tabela com data tables

--- src/script/historico.js
+++ src/script/historico.js
@@ -1,8 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    updateTable();
+
+    document.getElementById("selectLancamento").addEventListener("change", updateTable);
+    document.getElementById("selectPeriodo").addEventListener("change", updateTable);
+});
+
 var iconUser = document.getElementById("iconUser");
 var foto = document.getElementById("userpic");
-
-// Verificar se 'currentUser' existe no localStorage e se não é null
-let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+let currentUser = JSON.parse(localStorage.getItem("currentUser"));
 let profileImage = currentUser ? currentUser.profileImage : null;
 
 function mudarFoto() {
@@ -15,157 +20,156 @@ function mudarFoto() {
         foto.style.display = "none";
     }
 }
-
 mudarFoto();
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Chamar a função para atualizar a tabela ao carregar a página
-    updateTable();
+function updateTable() {
+    let tipoSelecionado = document.getElementById("selectLancamento").value;
+    let periodoSelecionado = document.getElementById("selectPeriodo").value;
+    let data = JSON.parse(localStorage.getItem("lançamentos")) || [];
 
-    document.getElementById("selectLancamento").addEventListener("change", function () {
-        updateTable();
-    });
-
-    document.getElementById("selectPeriodo").addEventListener("change", function () {
-        updateTable();
-    });
-
-    // Adicionar ouvintes de evento aos botões de edição
-    document.addEventListener("click", function(event) {
-        if (event.target.matches(".edit-button, .edit-button *")) {
-            let button = event.target.closest(".edit-button");
-            let index = button.getAttribute("data-index");
-            let data = JSON.parse(localStorage.getItem("lançamentos")) || [];
-            let item = data[index];
-            document.getElementById("modal-descricao").value = item.descricao;
-            document.getElementById("modal-valor").value = item.valor;
-            document.getElementById("modal-date").value = item.date;
-            document.getElementById("modal-categoria").value = item.categoria; // Assumindo que você tem uma categoria
-            document.getElementById("modal-descricao").setAttribute("data-index", index);
+    let filteredData = data.filter((item) => {
+        if (item.tipo === "button") {
+            return false;
+        }
+        if (tipoSelecionado === "1") {
+            return item.tipo === "Despesas";
+        } else if (tipoSelecionado === "2") {
+            return item.tipo === "Receitas";
+        } else {
+            return item.tipo === "Receitas" || item.tipo === "Despesas";
         }
     });
 
-    document.getElementById("template-btn").addEventListener("click", function() {
-        saveEdit();
-    });
-
-    document.getElementById("confirmDeleteButton").addEventListener("click", function() {
-        deleteItem();
-    });
-});
-
-
-function updateTable() {
-    // Obter o tipo selecionado
-    let tipoSelecionado = document.getElementById("selectLancamento").value;
-    // Obter o período selecionado
-    let periodoSelecionado = document.getElementById("selectPeriodo").value;
-
-    // Obter todos os dados da localStorage
-    let data = JSON.parse(localStorage.getItem("lançamentos")) || [];
-
-    // Filtrar os dados com base no tipo selecionado
-    let filteredData;
-    if (tipoSelecionado === "1") {
-        filteredData = data.filter((item) => item.tipo === "Despesas");
-    } else if (tipoSelecionado === "2") {
-        filteredData = data.filter((item) => item.tipo === "Receitas");
-    } else {
-        filteredData = data;
-    }
-
-    // Filtrar os dados com base no período selecionado
-    let currentDate = new Date(); // Data atual
-    let selectedDate = new Date(); // Data selecionada pelo usuário
+    let currentDate = new Date();
+    let selectedDate = new Date();
     if (periodoSelecionado === "1") {
-        selectedDate.setDate(currentDate.getDate() - 15); // Últimos 15 dias
+        selectedDate.setDate(currentDate.getDate() - 15);
     } else if (periodoSelecionado === "2") {
-        selectedDate.setDate(currentDate.getDate() - 30); // Últimos 30 dias
+        selectedDate.setDate(currentDate.getDate() - 30);
     } else if (periodoSelecionado === "3") {
-        selectedDate.setDate(currentDate.getDate() - 60); // Últimos 60 dias
+        selectedDate.setDate(currentDate.getDate() - 60);
     } else if (periodoSelecionado === "4") {
-        selectedDate.setDate(currentDate.getDate() - 90); // Últimos 90 dias
+        selectedDate.setDate(currentDate.getDate() - 90);
+    } else {
+        selectedDate = new Date(0);
     }
 
-    // Construir a tabela com os dados filtrados
     let tableBody = document.getElementById("tableBody");
-    // let html = "";
-    // filteredData.forEach((item, index) => {
-    //     let itemDate = new Date(item.date);
-    //     html += `<tr>`;
-    //     if (item.tipo === "Receitas") {
-    //         html += `<td class="text-center text-success">${item.descricao}</td>`;
-    //         html += `<td class="text-center text-success">${item.valor}</td>`;
-    //         html += `<td class="text-center text-success">${item.date}</td>`;
-    //         html += `<td class="text-center text-success">
-    //                 <button type="button" class="btn btn-sm template-btn edit-button" data-bs-toggle="modal" data-bs-target="#modalHistorico" data-index="${index}">
-    //                     <i class="bi bi-pencil-square"></i>
-    //                 </button>
-    //             </td>`;
-    //     } else if (item.tipo === "Despesas") {
-    //         html += `<td class="text-center text-danger">${item.descricao}</td>`;
-    //         html += `<td class="text-center text-danger">${item.valor}</td>`;
-    //         html += `<td class="text-center text-danger">${item.date}</td>`;
-    //         html += `<td class="text-center text-danger">
-    //                 <button type="button" class="btn btn-sm template-btn edit-button" data-bs-toggle="modal" data-bs-target="#modalHistorico" data-index="${index}">
-    //                     <i class="bi bi-pencil-square"></i>
-    //                 </button>
-    //             </td>`;
-    //     }
-    //     html += `</tr>`;
-    // });
     let html = "";
     filteredData.forEach((item, index) => {
         let itemDate = new Date(item.date);
-        html += `<tr>`;
-        if (item.tipo === "Receitas") {
-            html += `<td class="text-center text-success">${item.descricao}</td>`;
-            html += `<td class="text-center text-success">${item.valor}</td>`;
-            html += `<td class="text-center text-success">${item.date}</td>`;
-            html += `<td class="text-center text-success">
-                    <button type="button" class="btn btn-sm template-btn edit-button" data-bs-toggle="modal" data-bs-target="#modalHistorico" data-index="${index}">
-                        <i class="bi bi-pencil-square"></i>
-                    </button>
-                </td>`;
-        } else if (item.tipo === "Despesas") {
-            html += `<td class="text-center text-danger">${item.descricao}</td>`;
-            html += `<td class="text-center text-danger">${item.valor}</td>`;
-            html += `<td class="text-center text-danger">${item.date}</td>`;
-            html += `<td class="text-center text-danger">
-                    <button type="button" class="btn btn-sm template-btn edit-button" data-bs-toggle="modal" data-bs-target="#modalHistorico" data-index="${index}">
-                        <i class="bi bi-pencil-square"></i>
-                    </button>
-                </td>`;
+        if (itemDate >= selectedDate) {
+            html += '<tr>';
+            html += `<td class="text-center ${item.tipo === "Receitas" ? "text-success" : "text-danger"}">${item.descricao}</td>`;
+            html += `<td class="text-center ${item.tipo === "Receitas" ? "text-success" : "text-danger"}">${item.valor}</td>`;
+            html += `<td class="text-center ${item.tipo === "Receitas" ? "text-success" : "text-danger"}">${item.date}</td>`;
+            html += `<td class="text-center ${item.tipo === "Receitas" ? "text-success" : "text-danger"}">
+                        <button type="button" class="btn btn-sm template-btn edit-btn" data-bs-toggle="modal" data-bs-target="#modalHistorico" data-id="${data.indexOf(
+                            item
+                        )}">
+                            <i class="bi bi-pencil-square"></i>
+                        </button>
+                    </td>`;
+            html += '</tr>';
         }
-        html += `</tr>`;
+    });
+    tableBody.innerHTML = html;
+
+    document.querySelectorAll('[data-bs-target="#modalHistorico"]').forEach((button) => {
+        button.addEventListener("click", function () {
+            let recordId = this.getAttribute("data-id");
+            populateModal(recordId);
+        });
     });
 
-    // Atualizar o conteúdo da tabela na página
-    tableBody.innerHTML = html;
+    document.querySelectorAll('.edit-btn').forEach((button) => {
+        button.removeEventListener("click", showEditModal);
+        button.addEventListener("click", showEditModal);
+    });
 }
 
-function saveEdit() {
-    let index = document.getElementById("modal-descricao").getAttribute("data-index");
+function showEditModal() {
+    let recordId = this.getAttribute("data-id");
+    populateModal(recordId);
+}
+function populateModal(recordId) {
     let data = JSON.parse(localStorage.getItem("lançamentos")) || [];
-    data[index].descricao = document.getElementById("modal-descricao").value;
-    data[index].valor = document.getElementById("modal-valor").value;
-    data[index].date = document.getElementById("modal-date").value;
-    data[index].categoria = document.getElementById("modal-categoria").value;
+    let record = data[recordId];
+
+    if (record) {
+        document.getElementById("modal-descricao").value = record.descricao;
+        document.getElementById("modal-categoria").value = record.categoria;
+        document.getElementById("modal-valor").value = record.valor;
+        document.getElementById("modal-date").value = record.date;
+    }
+
+    document.getElementById("template-btn").onclick = function () {
+        salvarEdicao(recordId);
+    };
+
+    document.getElementById("excluir_btn").onclick = function () {
+        confirmarExclusao(recordId);
+    };
+}
+
+
+function confirmarExclusao(recordId) {
+    let confirmDeleteModal = new bootstrap.Modal(document.getElementById('confirmDeleteModal'));
+    let confirmDeleteButton = document.getElementById("confirmDeleteButton");
+
+    confirmDeleteButton.removeEventListener("click", handleDeleteConfirmation);
+    function handleDeleteConfirmation() {
+        excluirLancamento(recordId);
+        confirmDeleteModal.hide();
+        removeModalBackdrop();
+    }
+    confirmDeleteButton.addEventListener("click", handleDeleteConfirmation);
+
+    confirmDeleteModal.show();
+
+    let cancelDeleteButton = document.getElementById("cancelDeleteButton");
+    cancelDeleteButton.removeEventListener("click", hideConfirmDeleteModal);
+    function hideConfirmDeleteModal() {
+        confirmDeleteModal.hide();
+        removeModalBackdrop();
+    }
+    cancelDeleteButton.addEventListener("click", hideConfirmDeleteModal);
+}
+
+function excluirLancamento(recordId) {
+    let data = JSON.parse(localStorage.getItem("lançamentos")) || [];
+    data.splice(recordId, 1);
     localStorage.setItem("lançamentos", JSON.stringify(data));
     updateTable();
-    let modal = bootstrap.Modal.getInstance(document.getElementById('modalHistorico'));
-    modal.hide();
+    removeModalBackdrop();
 }
 
-function deleteItem() {
-    let index = document.getElementById("modal-descricao").getAttribute("data-index");
+function salvarEdicao(recordId) {
     let data = JSON.parse(localStorage.getItem("lançamentos")) || [];
-    data.splice(index, 1);
-    localStorage.setItem("lançamentos", JSON.stringify(data));
-    updateTable();
-    let deleteModal = bootstrap.Modal.getInstance(document.getElementById('confirmDeleteModal'));
-    deleteModal.hide();
-    let editModal = bootstrap.Modal.getInstance(document.getElementById('modalHistorico'));
-    editModal.hide();
+
+    let descricao = document.getElementById("modal-descricao").value;
+    let categoria = document.getElementById("modal-categoria").value;
+    let valor = document.getElementById("modal-valor").value;
+    let date = document.getElementById("modal-date").value;
+
+    if (recordId !== null && data[recordId]) {
+        data[recordId].descricao = descricao;
+        data[recordId].categoria = categoria;
+        data[recordId].valor = valor;
+        data[recordId].date = date;
+
+        localStorage.setItem("lançamentos", JSON.stringify(data));
+        updateTable();
+
+        let modal = bootstrap.Modal.getInstance(document.getElementById('modalHistorico'));
+        modal.hide();
+        removeModalBackdrop();
+    }
 }
 
+function removeModalBackdrop() {
+    let backdrops = document.querySelectorAll('.modal-backdrop');
+    backdrops.forEach((backdrop) => {
+        backdrop.parentNode.removeChild(backdrop);
+    });
+}
